f8072b511f70758e80ea37d5e8c086c1a2505395
Stefen Menzel <stuff@Stefens-MBP.lan>
2019-05-04 11:37:57 -0500

    got the conditional rendering working for the image descriptions

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,14 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/GGJ2018.jpg', description: 'Photo of my team from Global Game Jam 2018', likes:0},
+    { id: 3, path: 'images/guboo.jpg', description: 'The cutest pupper doggo that lives at my house', likes:1000},
+    { id: 4, path: 'images/LokiTheCat.jpg', description: 'The coolest cat, my roommate, and cuddlebuddy, Loki', likes:100},
+    { id: 5, path: 'images/mybeautifulhair.jpg', description: 'Me when my hair was at its longest', likes:0},
+    { id: 6, path: 'images/MeNBro.jpg', description: 'Me and my actual bro', likes:0},
+    { id: 7, path: 'images/Rhea.jpg', description: 'Rhea and I at a wedding', likes:0},
+    { id: 8, path: 'images/RheaAndMeBWCA.jpg', description: 'Rhea and I at the BWCA', likes: 0},
+    { id: 9, path: 'images/Sibs.jpg', description: 'My siblings and I', likes:0},
+    { id: 10, path: 'images/Moms.jpg', description: 'My mother and I', likes:0}
 ];
 
 module.exports = galleryItems;
--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,15 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class GalleryItem extends Component{
+class GalleryItem extends Component {
+
+    state = {
+        isClicked: false
+    }
 
     addLike = (idToLike) => {
         this.props.addLike(idToLike);
     }
 
-    render(){
-        return(
+    setClicked = () => {
+        this.setState({
+            isClicked: !this.state.isClicked
+        })
+    }
+
+    setImageOrText = () => {
+        return (
+            <div className="imageContainer">
+                <img className="gImage" onClick={this.setClicked} src={this.props.galleryItem.path} alt=''></img>
+                <div onClick={this.setClicked} className={this.state.isClicked ? 'imageOverlay' : 'imageGoAway'}>
+                    <div className="imageText">{this.props.galleryItem.description}</div>
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        console.log(this.state.isClicked);
+        return (
             <div className="gItem" >
-                <img className="gImage" src={this.props.galleryItem.path} alt=''></img>
+                {this.setImageOrText()}
                 <button className="gButton" onClick={() => this.addLike(this.props.galleryItem.id)}>LOVE IT!</button>
                 <p className="gP">Likes: {this.props.galleryItem.likes}</p>
             </div>
